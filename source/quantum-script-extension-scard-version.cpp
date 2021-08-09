bfe2a5425c3994379a2c80afae595dd69b3f6a66
Grigore Stefan <g_stefan@yahoo.com>
2021-08-09 15:44:28 +0300

    Update

--- source/quantum-script-extension-scard-version.cpp
+++ source/quantum-script-extension-scard-version.cpp
@@ -16,9 +16,9 @@ namespace Quantum {
 				namespace Version {
 
 					static const char *version_ = "2.0.0";
-					static const char *build_ = "8";
-					static const char *versionWithBuild_ = "2.0.0.8";
-					static const char *datetime_ = "2021-07-23 16:34:04";
+					static const char *build_ = "9";
+					static const char *versionWithBuild_ = "2.0.0.9";
+					static const char *datetime_ = "2021-08-05 20:38:19";
 
 					const char *version() {
 						return version_;
